7a43e00ae04279dc5045da811c359c3754a70f00
rsj2003 <rsj2003@naver.com>
2020-10-07 17:29:28 +0900

    format: 포맷 변경

--- src/Controller/User.php
+++ src/Controller/User.php
@@ -1,60 +1,69 @@
 <?php
+
 namespace src\Controller;
 // session_start();
 use src\Core\DB;
 
 class User
 {
-  public function login() {
-    view("login",[]);
+  public function login()
+  {
+    view("login", []);
   }
-  public function join() {
-    view("join",[]);
+
+  public function join()
+  {
+    view("join", []);
   }
-	function loginAction(){
-    $user = DB::fetch("SELECT * FROM users WHERE email = ? AND pass = ?",[$_POST["id"], $_POST["pass"]]);
 
-		if($user) {
-			$_SESSION["user"] = $user;
+  function loginAction()
+  {
+    $user = DB::fetch("SELECT * FROM users WHERE email = ? AND pass = ?", [$_POST["id"], $_POST["pass"]]);
 
-			move("/","로그인 되었습니다.");
-		}else {
+    if ($user) {
+      $_SESSION["user"] = $user;
+
+      move("/", "로그인 되었습니다.");
+    } else {
       back("아이디 또는 비밀번호가 일치하지 않습니다.");
-		}
-	}
+    }
+  }
 
-	function logout() {
-		unset($_SESSION["user"]);
-		move("/","로그아웃 되었습니다.");
-	}
+  function logout()
+  {
+    unset($_SESSION["user"]);
+    move("/", "로그아웃 되었습니다.");
+  }
 
-	function joinAction() {
+  function joinAction()
+  {
 //    print_r($_FILES["profile"]);
     $imageType = explode("/", $_FILES["profile"]["type"]);
-    if($imageType[0] !== "image") {
+    if ($imageType[0] !== "image") {
       return back("이미지 파일만 업로드 가능합니다.");
     }
 
-    if($imageType[1] !== "jpg" && $imageType[1] !== "png" && $imageType[1] !== "gif") {
+    if ($imageType[1] !== "jpg" && $imageType[1] !== "png" && $imageType[1] !== "gif") {
       return back("이미지 파일은 jpg, png, gif 파일만 업로드 가능합니다.");
     }
 
-    $imgName = $_POST["id"].time().".".$imageType[1];
+    $imgName = $_POST["id"] . time() . "." . $imageType[1];
 //		echo $imgName;
     $sql = "INSERT INTO `users`(`email`, `pass`, `name`, `birth`, `image`) VALUES (?,?,?,?,?)";
-    DB::query($sql,[$_POST["id"],$_POST["pass"],$_POST["name"],$_POST["birth"],$imgName]);
-    move_uploaded_file($_FILES["profile"]["tmp_name"], "./public/images/user_profile/".$imgName);
-    move("/","회원가입이 완료되었습니다.");
-	}
+    DB::query($sql, [$_POST["id"], $_POST["pass"], $_POST["name"], $_POST["birth"], $imgName]);
+    move_uploaded_file($_FILES["profile"]["tmp_name"], "./public/images/user_profile/" . $imgName);
+    move("/", "회원가입이 완료되었습니다.");
+  }
 
-	function emailCheck() {
-		$id = $_POST['id'];
-		$response = ["success" => true];
-		if (DB::fetch("SELECT * FROM users WHERE email = ?", [$id])) {
-			$response['success'] = false;
-		}
-		echo json_encode($response);
-	}
+  function emailCheck()
+  {
+    $id = $_POST['id'];
+    $response = ["success" => true];
+    if (DB::fetch("SELECT * FROM users WHERE email = ?", [$id])) {
+      $response['success'] = false;
+    }
+    echo json_encode($response);
+  }
 
 
 }
