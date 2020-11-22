46f1e5c404b5cf010959ec6e5f0ce04fdf218787
wonsunil <seonil6352@gmail.com>
2020-11-23 02:11:42 +0900

    Feat: add index.php file for mvc pattern

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+include_once("src/core/Route.php");
+include_once("src/core/People.php");
+include_once("src/core/Board.php");
+include_once("src/Controller/MainController.php");
+include_once("lib.php");
+
+use src\core\Route;
+
+Route::add("GET", "/webskills/", "MainController::main");
+Route::add("GET", "/webskills/main", "MainController::main");
+
+Route::run();
+
+?>
