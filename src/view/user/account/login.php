56ad705d3ded287fae7f3081bb23ba8c688075b8
wonsunil <seonil6352@gmail.com>
2020-11-23 22:25:28 +0900

    Refactor: change file structure

--- src/view/user/account/login.php
+++ src/view/user/account/login.php
@@ -1,48 +1,5 @@
 <?php
 
-include "function.php";
-include "../core/DB.php";
-
-use src\core\DB;
-
-$email = $_POST["email"];
-$password = $_POST["password"];
-
-login($email, $password);
-
-function login($email, $password) {
-	$id_check = false;
-	$pw_check = false;
-
-	$result = DB::fetch("select * from person where Email='$email' and Password=password('$password');", []);
-
-	if($result) {
-		session_start();
-
-		$_SESSION["email"] = $email;
-		$_SESSION["login-whether"] = true;
-
-		header('Location: /webskills/main.php');
-	}else{
-		$result = DB::fetch("select * from person where Email='$email';", []);
-
-		session_start();
-
-		$_SESSION["email"] = "";
-		$_SESSION["login-whether"] = false;
-
-		if($result) {
-			alert("비밀번호를 확인해주세요.");
-
-			echo "<script>document.location.href='/webskills/src/page/login.html';</script>";
-		}else {
-			alert("존재하지않는 아이디입니다.");
-
-			echo "<script>document.location.href='/webskills/src/page/login.html';</script>";
-		};
-	};
-};
-
 ?>
 
 <!DOCTYPE html>
@@ -50,18 +7,18 @@ function login($email, $password) {
 <head>
     <meta charset="UTF-8">
     <title>index</title>
-    <link rel="stylesheet" href="/webskills/src/page/css/login.css">
-    <script src="/webskills/src/page/js/login.js"></script>
+    <link rel="stylesheet" href="/webskills/src/resources/css/login.css">
+    <script src="/webskills/src/resources/js/login.js"></script>
 </head>
 <body>
 <div>
-    <form method="post" action="/webskills/src/account/login.php" name="login">
+    <form method="post" action="/webskills/src/user/account/login" name="login">
         <input type="text" name="email"><br>
         <input type="password" name="password"><br>
         <button type="submit">로그인</button>
     </form>
 
-    <button><a href="/webskills/src/page/register.html">회원가입</a></button>
+    <button><a href="/webskills/src/user/account/register">회원가입</a></button>
 </div>
 </body>
 </html>
