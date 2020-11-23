085006d175109c2a7776173d28b77a1fa8fa90dc
wonsunil <seonil6352@gmail.com>
2020-11-23 23:27:06 +0900

    Feat: controller

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 
 session_start();
 
-include_once("src/core/Route.php");
-include_once("src/core/DB.php");
-include_once("src/Controller/MainController.php");
 include_once("lib.php");
+include_once("src/core/DB.php");
+include_once("src/core/Route.php");
+include_once("src/Controller/MainController.php");
+include_once("src/Controller/UserController.php");
 
 use src\core\Route;
 
 Route::add("GET", "/webskills/", "MainController::goToMain");
 Route::add("GET", "/webskills/main", "MainController::main");
-Route::add("GET", "/user/login", "UserController::login");
+Route::add("GET", "/webskills/src/user/account/login", "UserController::getLogin");
+Route::add("GET", "/webskills/src/user/account/register", "UserController::getRegister");
+
+Route::add("POST", "/webskills/src/user/account/login", "UserController::postLogin");
+Route::add("POST", "/webskills/src/user/account/register", "UserController::postRegister");
 
 Route::run();
-
-?>
---
+++ src/Controller/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+class UserController{
+    public static function getLogin() {
+        return view("src/view/user/account/login.php", []);
+    }
+
+    public static function postLogin() {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        print($email);
+
+//        login($email, $password);
+
+        function login($email, $password) {
+            $id_check = false;
+            $pw_check = false;
+
+            $result = DB::fetch("select * from person where Email='$email' and Password=password('$password');", []);
+
+            if($result) {
+                session_start();
+
+                $_SESSION["email"] = $email;
+                $_SESSION["login-whether"] = true;
+
+                header('Location: /webskills/main.php');
+            }else{
+                $result = DB::fetch("select * from person where Email='$email';", []);
+
+                session_start();
+
+                $_SESSION["email"] = "";
+                $_SESSION["login-whether"] = false;
+
+                if($result) {
+                    alert("비밀번호를 확인해주세요.");
+
+                    echo "<script>document.location.href='/webskills/src/page/login.html';</script>";
+                }else {
+                    alert("존재하지않는 아이디입니다.");
+
+                    echo "<script>document.location.href='/webskills/src/page/login.html';</script>";
+                };
+            };
+        };
+    }
+
+    public static function getRegister() {
+        return view("src/view/user/account/register.php", []);
+    }
+
+    public static function postRegister() {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $name = $_POST["name"];
+        $birthday = $_POST["birthday"];
+        $img = $_FILES["img"]["name"];
+
+        register($email, $password, $name, $birthday, $img);
+
+        function register($email, $password, $name, $birthday, $img) {
+            $img_path = "../account/image/user/profile";
+
+            $imgs = $_FILES["img"]["name"];
+            if($imgs) {
+
+                $imgs = explode(".", $imgs);
+                $img_nm = cut_email($email)."_profile_img.".$imgs[1];
+
+                $sql_path = "../account/image/user/";
+                $sql_img_path = $sql_path.$img_nm;
+
+                DB::fetch("insert into person values('$email', password('$password'), '$name', '$birthday', '$sql_img_path', now());", []);
+
+                move_uploaded_file($_FILES["img"]["tmp_name"], $img_path.$img_nm);
+            }else {
+                DB::fetch("insert into person(Email, Password, Name, Birth, Time) values('$email', password('$password'), '$name', '$birthday', now());", []);
+            };
+
+            alert("회원가입 완료되었습니다");
+
+            echo "<script>document.location.href='/webskills/src/page/login.html';</script>";
+        };
+
+        function cut_email($email) {
+            $reg = '/@/';
+            preg_match($reg, $email,  $match, PREG_OFFSET_CAPTURE);
+
+            $result = substr($email, 0, $match[0][1]);
+
+            return $result;
+        };
+    }
+};
