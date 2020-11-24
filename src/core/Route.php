9af22eae29f0571ab7672da166754d87bd2a47c6
junilhwang <junil.h@kakao.com>
2020-11-24 22:04:18 +0900

    format: indent 변경

--- src/core/Route.php
+++ src/core/Route.php
@@ -5,22 +5,22 @@ namespace src\core;
 class Route{
     private static array $routes = [];
 
-	public static function add($method, $uri, $callback) {
+    public static function add($method, $uri, $callback) {
         $uriReg = str_replace("/", "\/", $uri ?? '/');
         $uriReg = "/^{$uriReg}$/";
-		self::$routes[] = compact("method", "uriReg", "callback");
-	}
+        self::$routes[] = compact("method", "uriReg", "callback");
+    }
 
-	public static function run() {
-	    $route = current(array_filter(self::$routes, fn($route) => self::match($route)));
+    public static function run() {
+        $route = current(array_filter(self::$routes, fn($route) => self::match($route)));
         preg_match_all($route['uriReg'], $_SERVER['REQUEST_URI'], $params, 0);
         $request = ['params' => $params];
-		return call_user_func($route["callback"], $request);
-	}
+        return call_user_func($route["callback"], $request);
+    }
 
-	private static function match($route) {
+    private static function match($route) {
         return $route['method'] === $_SERVER["REQUEST_METHOD"] &&
                preg_match($route['uriReg'], $_SERVER["REQUEST_URI"]);
-	}
+    }
 
 }
